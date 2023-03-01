0acd7dfe59d4a6548f0c9672f4008fb21dd0ae26
zhoujc <1258349367@qq.com>
2023-03-01 23:22:40 +0800

    feat:集成vue-router

--- src/main.js
+++ src/main.js
@@ -4,4 +4,6 @@ import App from './App.vue'
 import 'normalize.css'
 import './assets/css/index.css'
 
-createApp(App).mount('#app')
+import router from './router'
+
+createApp(App).use(router).mount('#app')
