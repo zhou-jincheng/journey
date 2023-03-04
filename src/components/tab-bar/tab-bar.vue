9fc5b92952671b6552726452b53189b1562a4f5a
zhoujc <1258349367@qq.com>
2023-03-04 09:17:12 +0800

    refactor:使用vant3-ui重写tab-bar组件

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,57 +1,32 @@
 <template>
   <div class="tabbar">
-    <template v-for="(item, index) in tabbarList" :key="item.text">
-      <div
-        :class="{active: currentIndex === index}"
-        class="tabbar-item"
-        @click="tabClick(index, item)">
-        <img :src="getAssetImageUrl(currentIndex === index ? item.imageActive : item.image)">
-        <span class="text">{{ item.text }}</span>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex">
+      <template v-for="(item, index) in tabbarList" :key="item.text">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon>
+            <img :src="getAssetImageUrl(currentIndex === index ? item.imageActive : item.image)">
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
+
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { useRouter } from 'vue-router'
 import tabbarList from '@/assets/data/tab-bar'
 import { getAssetImageUrl } from '@/utils/load_assets'
 
 const currentIndex = ref(0)
-const router = useRouter()
-function tabClick(index, item) {
-  currentIndex.value = index
-  router.push(item.path)
-}
 
 </script>
 
 <style lang="less" scoped>
 .tabbar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  border-top: 1px solid #eee;
-  display: flex;
-  .tabbar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    &.active {
-      color: var(--primary-color);
-    }
-    img {
-      width: 34px;
-    }
-    .text {
-      margin-top: 2px;
-      font-size: 12px;
-    }
+  img {
+    height: 26px;
   }
 }
 </style>
