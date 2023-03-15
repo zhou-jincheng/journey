5386f618a078455003c660156418d243c4f4eb93
zhoujc <1258349367@qq.com>
2023-03-15 23:48:03 +0800

    feat:完成详情页信息模块

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -6,7 +6,10 @@
       left-arrow
       @click-left="$router.back()"
     />
-    <home-swipe v-if="topModule.housePicture" :swipe-data="topModule.housePicture.housePics" />
+    <div v-if="topModule" class="main">
+      <detail-swipe :swipe-data="topModule.housePicture.housePics" />
+      <detail-infos :top-infos="topModule"/>
+    </div>
   </div>
 </template>
 
@@ -14,10 +17,12 @@
 import { useRoute } from 'vue-router';
 import { ref } from 'vue';
 import { getDetailInfos } from '@/services/modules/detail';
-import HomeSwipe from './cpns/home-swipe.vue'
+import DetailSwipe from './cpns/detail_01-swipe.vue'
+import DetailInfos from './cpns/detail_02-infos.vue'
+
 
 const route = useRoute()
-const topModule = ref({})
+const topModule = ref()
 getDetailInfos(route.params.id).then(res => {
   const mainPart = res.data.mainPart
   topModule.value = mainPart.topModule
