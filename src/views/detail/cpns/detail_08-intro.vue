198da551fafc5accfc2ef96b0d43f07586c7dbf3
zhoujc <1258349367@qq.com>
2023-03-22 21:20:28 +0800

    feat:完成详情页面价格说明模块

---
+++ src/views/detail/cpns/detail_08-intro.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="detail-price-intro">
+    <h3 class="title">{{ introduction.title }}</h3>
+    <div class="desc">{{ introduction.introduction }}</div>
+    <div class="footer">
+      <img src="@/assets/img/detail/icon_ensure.png" alt="">
+      <sapn class="text">宏源旅途，永无止尽!</sapn>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  introduction: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.detail-price-intro {
+  border-top: 5px solid #f2f3f4;
+  padding: 25px 15px 0;
+  .title {
+    font-size: 14px;
+  }
+  .desc {
+    margin-top: 12px;
+    font-size: 12px;
+    color: #666;
+    line-height: 1.5;
+  }
+  .footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 12px;
+    width: 100%;
+    height: 110px;
+    img {
+      width: 128px;
+    }
+    .text {
+      margin-top: 12px;
+      font-size: 12px;
+      color: #7688A7;
+    }
+  }
+}
+</style>
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -14,6 +14,7 @@
       <detail-comment :comment="mainPart.dynamicModule.commentModule" />
       <detail-notice :order-rules="mainPart.dynamicModule.rulesModule.orderRules"/>
       <detail-map :position="mainPart.dynamicModule.positionModule"/>
+      <detail-intro :introduction="mainPart.introductionModule" />
     </template>
   </div>
 </template>
@@ -29,6 +30,7 @@ import DetailLandlord from './cpns/detail_04-landlord.vue'
 import DetailComment from './cpns/detail_05-comment.vue'
 import DetailNotice from './cpns/detail_06-notice.vue'
 import DetailMap from './cpns/detail_07-map.vue'
+import DetailIntro from './cpns/detail_08-intro.vue'
 
 const route = useRoute()
 const mainPart = ref()
